89a449c3fe523172bd0596d4a3ffd858d2407b2f
mjsull <mjsull@gmail.com>
2020-06-08 22:21:57 +1000

    minor fix

---
+++ variant_analysis.py
@@ -0,0 +1,99 @@
+import sys, os, argparse, subprocess
+
+
+def run_variant_analysis(args):
+    sample_folder = args.sample_folder
+    sample = os.path.basename(sample_folder.rstrip('/'))
+    threads = args.threads
+    outdir = sample_folder + '/variants'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    read_1 = "%s/pipeline/combined.1.fastq.gz" % sample_folder
+    read_2 = "%s/pipeline/combined.2.fastq.gz" % sample_folder
+    fasta = sample_folder + '/pipeline/' + sample + '.fasta'
+    # subprocess.Popen("minimap2 -t %s -ax sr %s %s %s | samtools view -b | samtools sort -@ %s -o %s/ref.bam -"
+    #                  " && samtools index %s/ref.bam"
+    #                  % (threads, fasta, read_1, read_2, args.threads, outdir, outdir), shell=True).wait()
+    # subprocess.Popen("samtools mpileup -f %s %s/ref.bam > %s/pileup" % (fasta, outdir, outdir), shell=True).wait()
+    modlist = ['a', 't', 'c', 'g', 'n', 'I', 'D']
+    outseq = ''
+    with open("%s/pileup" % outdir) as f, open("%s/variable_bases.tsv" % outdir, "w") as out:
+        out.write("reference\tposition\treference_base\tcoverage\reference_base_fraction\tflagged\ta\tt\tc\tg\tn\tinsertion\tdeletion\n")
+        for line in f:
+            ref, pos, refbase, cov, seq, qual = line.split()
+            refbase = refbase.lower()
+            seq = seq.lower()
+            counts = {'a':0, 't':0, 'c':0, 'g':0, 'n':0, 'I':0, 'D':0}
+            depth = 0
+            seq = list(seq)
+            getdel = False
+            getins = False
+            while seq != []:
+                x = seq.pop(0)
+                mod = None
+                if x == '.' or x == ',':
+                    mod = refbase
+                elif x == '+':
+                    getins = True
+                    digit = ''
+                elif x == '-':
+                    getdel = True
+                    digit = ''
+                elif x.isdigit() and (getdel or getins):
+                    digit += x
+                elif getdel:
+                    for j in range(int(digit) -1):
+                        seq.pop(0)
+                    mod = 'D'
+                    getdel = False
+                elif getins:
+                    for j in range(int(digit) -1):
+                        seq.pop(0)
+                    mod = 'I'
+                    getins = False
+                elif x == '^':
+                    seq.pop(0)
+                elif x in ['a', 't', 'c', 'g']:
+                    mod = x
+                elif x in ['$', '*']:
+                    pass
+                else:
+                    sys.exit("Base not recognised " + x)
+                if not mod is None:
+                    counts[mod] += 1
+                    depth += 1
+            fraction = counts[refbase]/depth
+            if fraction < args.min_ratio:
+                flagged = "FLAGGED"
+                outseq += 'n'
+            else:
+                flagged = "OK"
+                outseq += refbase
+            outlist = [ref, pos, refbase, cov, fraction, flagged]
+            for i in modlist:
+                outlist.append(counts[i])
+            out.write('\t'.join(map(str, outlist)) + '\n')
+    with open("%s/%s.final.fna" % (outdir, sample), "w") as out:
+        out.write(">%s\n" % sample)
+        for i in range(0, len(outseq), 80):
+            out.write(outseq[i:i+80] + '\n')
+
+
+
+
+__version__ = "0.1.1"
+parser = argparse.ArgumentParser(prog='COVID variant pipeline', formatter_class=argparse.RawDescriptionHelpFormatter,
+                                description='QC for the assembly, mapping, base calling of viruses\n' \
+                                            'Version: %s\n' 
+                                            'License: GPLv3\n'
+                                            'USAGE: python run_QC.py -i sample1' % __version__)
+
+
+parser.add_argument('-i', '--sample_folder', action='store', help='Sample folder created by process_run.py')
+parser.add_argument('-t', '--threads', action='store', default="12", help='number of threads to use')
+parser.add_argument('-m', '--min_ratio', action='store', default=0.66, help='number of threads to use')
+
+
+
+args = parser.parse_args()
+run_variant_analysis(args)
