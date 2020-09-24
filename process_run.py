9ba928ac73f750b955a6413b690e0d028ce0d179
mjsull <mjsull@gmail.com>
2020-09-25 01:22:34 +1000

    updates to process sample script

--- process_run.py
+++ process_run.py
@@ -10,7 +10,7 @@ def process_reads(args):
 
     for i in os.listdir(args.input_folder):
         if os.path.isdir(os.path.join(args.input_folder, i)):
-            sample_name = i.split('_')[0]
+            sample_name = i.rstrip('/')
             gotr1 = False
             gotr2 = True
             for j in os.listdir(os.path.join(args.input_folder, i)):
@@ -32,7 +32,7 @@ def process_reads(args):
 __version__ = "0.1.1"
 parser = argparse.ArgumentParser(prog='COVID pipeline', formatter_class=argparse.RawDescriptionHelpFormatter,
                                 description="""process_run - process an Illumina sars-ncov-2 run.
-    USE: looks for folders with a pair of reads in them, interprets everything before the first underscore as a sample name
+    USE: looks for folders with a pair of reads in them, interpre   ts everything before the first underscore as a sample name
     then copies directories into output folder under sample name.
     USAGE: python process_run.py <illumina_run_dir> <output_dir>""")
 
@@ -40,8 +40,8 @@ parser = argparse.ArgumentParser(prog='COVID pipeline', formatter_class=argparse
 
 parser.add_argument('-i', '--input_folder', action='store', help='place where illumina reads are located')
 parser.add_argument('-o', '--output_folder', action='store', default="temp", help='output directory')
-parser.add_argument('-r1', '--read1_suffix', action='store', default="_1.fastq.gz", help='suffix for finding read 1')
-parser.add_argument('-r2', '--read2_suffix', action='store', default="_2.fastq.gz", help='suffix for finding read 2')
+parser.add_argument('-r1', '--read1_suffix', action='store', default="R1_001.fastq.gz", help='suffix for finding read 1')
+parser.add_argument('-r2', '--read2_suffix', action='store', default="R2_001.fastq.gz", help='suffix for finding read 2')
 parser.add_argument('-v', '--version', action='store_true', help="print version and exit")
 
 
